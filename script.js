29e7258a49f3ec244db58977c765f737f71483fe
DennisMeide <66964901+DennisMeide@users.noreply.github.com>
2022-05-13 21:52:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ function addCode(pName, pVorname, ownText, quote, specialMark) {
   }
 }
 
-addCode("Meide","Dennis","Ist nicht aufmerksam.", "Liegt Deutschland nicht in Berlin?", true);
-addCode("Person","Test","Lernt noch aus", "Ich hasse es, zitiert zu werden!", false);
+const studentList = [
+
+  ["Meide","Dennis","Liebt Katzen und Pizza. Aber nicht Katzen auf Pizza.","Ist Deutschland nicht in Berlin?",true],
+  ["Fenn","Nate","Ist gut in Geographie, weiss aber nicht wo Kaukasus liegt.","Kaukaasus",false],
+  ["Saijkosky","Tim Elias","Hasst Schule. Hasst jeden.","Ich hasse dich auch. ",false],
+  ["Kolisch","Garret","Hat interessante politische Ansichten.","Hallo!",false]
+
+];
+
+studentList.sort();
+
+for (let i = 0 ; i < studentList.length ; i++ ) {
+  addCode(studentList[i][0],studentList[i][1],studentList[i][2], studentList[i][3], studentList[i][4]);
+};
