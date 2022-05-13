d37f948c59adc39385d956935b6d7e259a1b10fc
DennisMeide <66964901+DennisMeide@users.noreply.github.com>
2022-05-13 12:55:12 +0200

    test for js

---
+++ script.js
@@ -0,0 +1,16 @@
+const schueler = {
+    name: "Meide",
+    firstname: "Dennis",
+    personalText: "Das hier ist dennis. Er kann nicht Programmieren",
+    qoute: "Liegt Deutschland nicht in Berlin?"
+};
+
+const display =`
+    <div class="student">
+        <p class="name">${schueler.name}, ${schueler.firstname}</p>
+        <p>${schueler.personalText}</p>
+        <p>${schueler.qoute}</p>
+    </div>
+`;
+
+document.getElementsByClassName("students").innerHTML= display;
