8ee914fa10863fcbc56d124b13b9b47260889ad0
DennisMeide <66964901+DennisMeide@users.noreply.github.com>
2022-05-22 01:15:51 +0200

    changing everything

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-function addCode(pName, pVorname, ownText, quote, specialMark) {
-  if(!specialMark) {
-    document.getElementById("students").innerHTML += '<div class="student"><p class="name">' + pName + ', ' + pVorname + '</p><p>' + ownText + '</p><p class="zitat">"' + quote + '"</p></div>';
-  } else {
-    document.getElementById("students").innerHTML += '<div class="student pointOut"><p class="name">' + pName + ', ' + pVorname + ' - SV</p><p>' + ownText + '</p><p class="zitat">"' + quote + '"</p></div>';
+function addCode(pNr) {
+    document.getElementById("pictureframe").innerHTML += '<div class="pict"><img class="img" src="./img_src/' + pNr + '.jpeg"></div>';
   }
-}
-
-
-// --------------DELETE LATER------------------------------------------------------------------------------
-
-alert("Wenn du Vorschläge für einen Text, Bilder, oder eine Änderung (z.B. Tippfehler) hast, kontaktiere Dennis Meide oder die SV!");
-
-// --------------DELETE LATER------------------------------------------------------------------------------
-
-
-
-const studentList = [
-
-  ["Meide","Dennis","Liebt Katzen, Pizza, und seine Freundin.","Ist Deutschland nicht in Berlin?",false]
   
-];
-
-studentList.sort();
-
-for (let i = 0 ; i < studentList.length ; i++ ) {
-  addCode(studentList[i][0],studentList[i][1],studentList[i][2], studentList[i][3], studentList[i][4]);
-};
+  
+  // --------------DELETE LATER------------------------------------------------------------------------------
+  
+  //alert("Wenn du Vorschläge für einen Text oder eine Änderung (z.B. Tippfehler) hast, kontaktiere Dennis Meide oder die SV!");
+  
+  // --------------DELETE LATER------------------------------------------------------------------------------
+  
+  const x = 18; //anzahl der bilder
+  
+  for (let i = 1 ; i <= x ; i++ ) {  
+    addCode(i.toString());
+  };
