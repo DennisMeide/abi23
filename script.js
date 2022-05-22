9284d531ba3e6ecaa0d035f28856e6a3804cb116
DennisMeide <66964901+DennisMeide@users.noreply.github.com>
2022-05-22 13:05:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function addCode(pNr) {
+  function addPict(pNr, pHide) {
     document.getElementById("pictureframe").innerHTML += '<div class="pict"><img class="img" src="./img_src/' + pNr + '.jpeg"></div>';
+  }
+
+  function addStudent(pName, pFirstName, pText, pQoute, pMark, pHide) {
+    document.getElementById("studentListInHTML").innerHTML += '<div class="single_person_card"><h3>' + pName + ', ' + pFirstName + '</h3><p>' + pText + '</p></div>';
   }
   
   
@@ -10,7 +14,24 @@ function addCode(pNr) {
   // --------------DELETE LATER------------------------------------------------------------------------------
   
   const x = 18; //anzahl der bilder
+
+  try {
+    for (let i = 1 ; i <= x ; i++ ) {  
+      addPict(i.toString());
+    };
   
-  for (let i = 1 ; i <= x ; i++ ) {  
-    addCode(i.toString());
-  };
+  } catch (e) {
+    
+    const studentList = [
+      ["Meide","Dennis","Ist sehr beschäftigt damit, diese Seite hübsch aussehen zu lassen. Ist nicht sehr erfolgreich.","Deutschland liegt doch in Berlin?",false,false],
+      ["Name","FirstName","Text","Qoute",false,false],
+      ["Name","FirstName","Text","Qoute",false,false],
+      ["Name","FirstName","Text","Qoute",false,false],
+      ["Name","FirstName","Text","Qoute",false,false]
+    ];
+    for (let i = 0 ; i < studentList.length ; i++ ) {
+      addStudent(studentList[i][0],studentList[i][1],studentList[i][2], studentList[i][3], studentList[i][4], studentList[i][5]);
+    };
+  }
+  
+
